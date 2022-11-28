485c54d6cdb1c5689436dc18340b7300cc57a93b
Tiaan Kemp <tiaan.kemp@ap.omron.net>
2022-11-28 16:58:29 +1300

    4:58 28/11/2022

--- Udemy/01-starting-setup/src/Components/Expenses/Expenses.js
+++ Udemy/01-starting-setup/src/Components/Expenses/Expenses.js
@@ -1,34 +1,43 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
-function Expenses(props){
+function Expenses(props) {
+  const [filterSelected, onSelectedFilter] = useState("2023");
 
-    return (
-      //Card act as a wrapper
-        <Card className="expenses">
-          <ExpenseItem 
-            date={props.expense_array[0].date}
-            title={props.expense_array[0].title}
-            amount={props.expense_array[0].amount}
-          />
-          <ExpenseItem
-            date={props.expense_array[1].date}
-            title={props.expense_array[1].title}
-            amount={props.expense_array[1].amount}
-          />
-          <ExpenseItem
-            date={props.expense_array[2].date}
-            title={props.expense_array[2].title}
-            amount={props.expense_array[2].amount}
-          />
-          <ExpenseItem
-            date={props.expense_array[3].date}
-            title={props.expense_array[3].title}
-            amount={props.expense_array[3].amount}
-          />
-        </Card>
-      );
+  function selectedFilterHandler(yearSelected) {
+    console.log(yearSelected);
+    onSelectedFilter(yearSelected);
+  }
+
+  return (
+    //Card act as a wrapper
+    <Card className="expenses">
+      <ExpensesFilter selected={filterSelected} onFilterSelect={selectedFilterHandler} />
+      <ExpenseItem
+        date={props.expense_array[0].date}
+        title={props.expense_array[0].title}
+        amount={props.expense_array[0].amount}
+      />
+      <ExpenseItem
+        date={props.expense_array[1].date}
+        title={props.expense_array[1].title}
+        amount={props.expense_array[1].amount}
+      />
+      <ExpenseItem
+        date={props.expense_array[2].date}
+        title={props.expense_array[2].title}
+        amount={props.expense_array[2].amount}
+      />
+      <ExpenseItem
+        date={props.expense_array[3].date}
+        title={props.expense_array[3].title}
+        amount={props.expense_array[3].amount}
+      />
+    </Card>
+  );
 }
 
 export default Expenses;
